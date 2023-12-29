22490e90bd83806998c37bbedeeef01b55c0e8dd
Somesh Pundir <104303129+SomeshPundir@users.noreply.github.com>
2023-12-29 18:32:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Counter App</h1>
+        <div>
+          <p>Count: {count}</p>
+          <button onClick={this.handleIncrement}>Increment</button>
+          <button onClick={this.handleDecrement}>Decrement</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
